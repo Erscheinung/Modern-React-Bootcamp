3ff45883ca7b76c928e851214bc182d43977f3fd
Erscheinung <kc1.408596@gmail.com>
2020-10-04 00:17:53 +0530

    add hangman

---
+++ rosarium-philosophorum/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Story from './Story';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Story/>
+      </div>
+    );
+  }
+
+}
+
+export default App;

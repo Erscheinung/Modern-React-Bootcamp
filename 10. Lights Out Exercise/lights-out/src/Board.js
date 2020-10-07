2358d370bd24f000c711f0fa3a68279b62593b22
Erscheinung <kc1.408596@gmail.com>
2020-10-07 17:30:58 +0530

    add exercise

--- 10. Lights Out Exercise/lights-out/src/Board.js
+++ 10. Lights Out Exercise/lights-out/src/Board.js
@@ -30,18 +30,30 @@ import './Board.css';
  **/
 
 class Board extends Component {
-
+  static defaultProps = {
+    nrows : 5,
+    ncols: 5,
+    chanceLightStartsOn: 0.25
+  };
   constructor(props) {
     super(props);
-
-    // TODO: set initial state
+    this.state = {
+      hasWon: false,
+      board: this.createBoard()
+    }
   }
 
   /** create a board nrows high/ncols wide, each cell randomly lit or unlit */
 
   createBoard() {
     let board = [];
-    // TODO: create array-of-arrays of true/false values
+    for (let y =0; y < this.props.nrows; y++) {
+      let row = [];
+      for (let x=0; x < this.props.ncols; x++) {
+        row.push(Math.random() < this.props.chanceLightStartsOn)
+      }
+      board.push(row);
+    }
     return board
   }
 
@@ -61,11 +73,16 @@ class Board extends Component {
       }
     }
 
-    // TODO: flip this cell and the cells around it
+    // flip this cell and the cells around it
+    flipCell(y,x);
+    flipCell(y,x-1);
+    flipCell(y,x+1);
+    flipCell(y-1,x);
+    flipCell(y+1,x);
 
     // win when every cell is turned off
-    // TODO: determine is the game has been won
-
+    // determine is the game has been won
+    let hasWon = board.every(row => row.every(cell => !cell))
     this.setState({board, hasWon});
   }
 
@@ -73,14 +90,21 @@ class Board extends Component {
   /** Render game board or winning message. */
 
   render() {
-
-    // if the game is won, just show a winning msg & render nothing else
-
-    // TODO
-
-    // make table board
-
-    // TODO
+    let tblBoard = [];
+    for (let y = 0; y < this.props.nrows; y++ ) {
+      let row = [];
+      for(let x =0; x < this.props.ncols; x++) {
+        let coord = `${y}-${x}`
+        row.push(<Cell key={coord} isLit={this.state.board[y][x]}
+        flipCellsAroundMe={() => this.flipCellsAround(coord)}/>);
+      }
+      tblBoard.push(<tr key={y}>{row}</tr>);
+    }
+    return (
+      <table className="Board">
+        <tbody>{tblBoard}</tbody>
+      </table>
+    )
   }
 }
 

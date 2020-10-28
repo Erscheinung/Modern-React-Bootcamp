8211ef735e60aede5a9f1f76791c5a84f4d27de2
Erscheinung <kc1.408596@gmail.com>
2020-10-28 23:34:59 +0530

    adding and removing forms

--- 12. Todo app/todo/src/NewTodoForm.js
+++ 12. Todo app/todo/src/NewTodoForm.js
@@ -1,11 +1,10 @@
 import React, { Component } from "react";
 import uuid from "uuid/v4";
-import "./NewTodoForm.css";
 
 class NewTodoForm extends Component {
   constructor(props) {
     super(props);
-    this.state = { task: "" };
+    this.state = { task: ""};
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
@@ -16,21 +15,21 @@ class NewTodoForm extends Component {
   }
   handleSubmit(evt) {
     evt.preventDefault();
-    this.props.createTodo({ ...this.state, id: uuid(), completed: false });
-    this.setState({ task: "" });
+    this.props.createTodo({...this.state, id: uuid()});
+    this.setState({ task: ""});
   }
   render() {
     return (
-      <form className='NewTodoForm' onSubmit={this.handleSubmit}>
+      <form onSubmit={this.handleSubmit}>
         <label htmlFor='task'>New Todo</label>
-        <input
-          type='text'
-          placeholder='New Todo'
-          id='task'
+        <input 
+          type='text' 
+          placeholder='New Todo' 
+          id='task' 
           name='task'
-          value={this.state.task}
-          onChange={this.handleChange}
-        />
+          value={this.state.task} 
+          onChange={this.handleChange}>
+        </input>
         <button>Add Todo</button>
       </form>
     );
--- 12. Todo app/todo/src/TodoList.js
+++ 12. Todo app/todo/src/TodoList.js
@@ -1,20 +1,39 @@
 import React, { Component } from 'react'
 import Todo from './Todo';
+import NewTodoForm from './NewTodoForm'
 
 class TodoList extends Component {
   constructor(props) {
     super(props);
-    this.state = {todos: [{task: "Split the psyche"},{task: "Speak to the spirit of the times"}]}
+    this.state = {
+      todos: []
+    };
+    this.create = this.create.bind(this);
+    this.remove = this.remove.bind(this);
+  }
+  create(newTodo) {
+    this.setState({
+      todos: [...this.state.todos, newTodo]
+    });
+  }
+  remove(id) {
+    this.setState({
+      todos: this.state.todos.filter(t => t.id !== id)
+    });
   }
   render() {
     const todos = this.state.todos.map(todo =>{
-      return <Todo task={todo.task} />;
+      return <Todo 
+        key={todo.id} 
+        task={todo.task} 
+        id={todo.id} 
+        removeTodo={this.remove}/>;
     });
     return (
       <div>
         <h1>Todo List!</h1>
         <ul>
-          <li>Todo 1</li>
+          <li><NewTodoForm createTodo={this.create}/></li>
           <li>{todos}</li>
         </ul>
       </div>

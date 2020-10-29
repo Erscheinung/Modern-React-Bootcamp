69e234bb2f3e96936d259b0806f238622513665c
Erscheinung <kc1.408596@gmail.com>
2020-10-29 22:42:13 +0530

    handle editing and completion

--- 12. Todo app/todo/src/TodoList.js
+++ 12. Todo app/todo/src/TodoList.js
@@ -10,6 +10,8 @@ class TodoList extends Component {
     };
     this.create = this.create.bind(this);
     this.remove = this.remove.bind(this);
+    this.update = this.update.bind(this);
+    this.toggleCompletion = this.toggleCompletion.bind(this);
   }
   create(newTodo) {
     this.setState({
@@ -21,13 +23,35 @@ class TodoList extends Component {
       todos: this.state.todos.filter(t => t.id !== id)
     });
   }
+  update(id, updatedTask){
+    const updatedTodos = this.state.todos.map(todo => {
+      if(todo.id === id){
+        return {...todo, task: updatedTask};
+      }
+      return todo;
+    });
+    this.setState({todos: updatedTodos});
+  }
+  toggleCompletion(id){
+    const updatedTodos = this.state.todos.map(todo => {
+      if(todo.id === id){
+        return {...todo, completed: !todo.completed};
+      }
+      return todo;
+    });
+    this.setState({todos: updatedTodos});
+  }
   render() {
     const todos = this.state.todos.map(todo =>{
       return <Todo 
         key={todo.id} 
         task={todo.task} 
+        completed={todo.completed}
         id={todo.id} 
-        removeTodo={this.remove}/>;
+        removeTodo={this.remove}
+        updateTodo={this.update}
+        toggleTodo={this.toggleCompletion}
+      />;
     });
     return (
       <div>
